7dd6d0e9c35395ab2caab135c4c888364706c4dc
Akash Gupta <akash@adias.tech>
2020-05-04 17:09:51 +0530

    validation pipe added to encfore validation rules

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Used Validation pipe to work with class-validator
+   * To know more about validation pipe go here {@link https://docs.nestjs.com/techniques/validation}
+   */
+  app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(3000);
 }
 bootstrap();
