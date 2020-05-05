cb47599425f7ed5adaa4e7fa3a0f79b2f6d90148
kancherlakishorereddy <kishorereddykancherla@gmail.com>
2020-05-05 11:50:32 +0530

    file upload

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,29 @@
-import { Controller, Get, UseGuards, Req } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Req,
+  Post,
+  UseInterceptors,
+  UploadedFile,
+} from "@nestjs/common";
+import { AppService } from "./app.service";
+import { AuthGuard } from "@nestjs/passport";
+import { FileInterceptor } from "@nestjs/platform-express";
 
 @Controller()
+@UseGuards(AuthGuard())
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @UseGuards(AuthGuard())
   getHello(@Req() req: any): string {
     return this.appService.getHello(req.user);
+  }
+
+  @Post("upload")
+  @UseInterceptors(FileInterceptor("file"))
+  uploadFile(@UploadedFile() file) {
+    console.log(file);
   }
 }
